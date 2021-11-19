8be76f1149b6f1ebdea4b7d6427785231c825465
taj1-takum1 <takumin390511@gmail.com>
2021-11-19 16:22:45 +0900

    routeオブジェクトからパラメーターを取得

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav>
+      <router-link to="/" class="link" active-class="link--active" exact>Home</router-link>
+      <router-link to="/users" class="link" active-class="link--active" exact>Users</router-link>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.link {
+  margin-right: 10px;
+}
+.link--active {
+  font-size: 20px;
 }
 </style>
