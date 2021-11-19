af35d03e679085d090f7dcb08b5c567ba6be468f
taj1-takum1 <takumin390511@gmail.com>
2021-11-19 17:26:32 +0900

    名前付きルート

--- src/router.js
+++ src/router.js
@@ -16,7 +16,7 @@ export default new Router({
       props: true,
       children: [
         { path: "posts", component: UsersPosts },
-        { path: "profile", component: UsersProfile },
+        { path: "profile", component: UsersProfile, name: "users-id-profile" },
       ]
     }
   ]
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -5,6 +5,8 @@
     <router-link to="/users/2">ユーザー2</router-link>
     <hr>
     <h1>User No. {{ id }}</h1>
+    <router-link :to="'/users/' + (Number(id) + 1) + '/profile'">次のユーザー</router-link>
+    <router-link :to="{ name: 'users-id-profile', params: { id: Number(id) + 1 } }">次のユーザー</router-link>
     <router-view></router-view>
   </div>
 </template>
