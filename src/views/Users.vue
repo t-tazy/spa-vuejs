d326d2dcbd53de75924e4c224b98d19aa05d9f23
taj1-takum1 <takumin390511@gmail.com>
2021-11-19 16:34:41 +0900

    watcherを使ってパラメーターが変更された時の処理を記述

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
     <h3>Users</h3>
+    <router-link to="/users/1">ユーザー1</router-link>
+    <router-link to="/users/2">ユーザー2</router-link>
     <hr>
     <h1>User No. {{ $route.params.id }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  watch: {
+    $route(to, from) {
+      console.log(to);
+      console.log(from);
+    }
+  }
+};
+</script>
