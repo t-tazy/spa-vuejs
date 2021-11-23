5e2165825aa89ad4292d7d97cb771491d63a11f4
t-tazy <takumin390511@gmail.com>
2021-11-23 12:31:57 +0900

    名前付きビュー

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <template>
-  <div>
-    <nav>
-      <router-link to="/" class="link" active-class="link--active" exact>Home</router-link>
-      <router-link to="/users" class="link" active-class="link--active" exact>Users</router-link>
-    </nav>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <router-view name="header"></router-view>
     <router-view></router-view>
   </div>
 </template>
 
-<style>
-.link {
-  margin-right: 10px;
-}
-.link--active {
-  font-size: 20px;
-}
-</style>
---
+++ src/views/HeaderHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <h2>Home</h2>
+    <router-link to="/" class="link" active-class="link--active" exact>Home</router-link>
+    <router-link to="/users" class="link" active-class="link--active" exact>Users</router-link>
+  </nav>
+</template>
+
+<style>
+.link {
+  margin-right: 10px;
+}
+.link--active {
+  font-size: 20px;
+}
+</style>
