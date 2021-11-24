df4eac779b6d0336e4f0c1e3a24984594e4504e7
t-tazy <takumin390511@gmail.com>
2021-11-24 13:27:31 +0900

    mapMutations

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -3,20 +3,16 @@
     <h2>Home</h2>
     <router-link to="/" class="link" active-class="link--active" exact>Home</router-link>
     <router-link to="/users" class="link" active-class="link--active" exact>Users</router-link>
-    <button @click="increment">+1</button>
-    <button @click="decrement">-1</button>
+    <button @click="increment(2)">+1</button>
+    <button @click="decrement(2)">-1</button>
   </nav>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
 export default {
   methods: {
-    increment() {
-      this.$store.commit('increment', 2);
-    },
-    decrement() {
-      this.$store.commit('decrement', 2);
-    }
+    ...mapMutations(["increment", "decrement"])
   }
 }
 </script>
